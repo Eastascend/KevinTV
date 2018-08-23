414d95f6cf8215ed6aaa8773aff7547f0c40da07
Eastascend <947747330@qq.com>
2018-08-23 21:11:31 +0800

    FirstRelease

---
+++ application/extra/version.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+    'name' => '苹果CMS',
+    'copyright' => 'MacCMS.COM',
+    'url' => 'http://www.maccms.com/',
+    'code' => '2018.07.29.1010',
+    'license' => '免费版',
+);
+?>
